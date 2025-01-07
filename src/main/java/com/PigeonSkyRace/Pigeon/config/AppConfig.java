f8c15305546de664b7bde5c93f360faf955c5d76
Khadija2504 <khadijalady25@gmail.com>
2025-01-07 16:15:43 +0100

    disable the ssl certificate and update the user dto and user modul and app config to configur the angular frontend consummation

--- src/main/java/com/PigeonSkyRace/Pigeon/config/AppConfig.java
+++ src/main/java/com/PigeonSkyRace/Pigeon/config/AppConfig.java
@@ -4,6 +4,8 @@ import com.PigeonSkyRace.Pigeon.security.JwtRequestFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -14,5 +16,17 @@ public class AppConfig {
         registrationBean.setFilter(filter);
         registrationBean.addUrlPatterns("/api/breeder/*");
         return registrationBean;
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
